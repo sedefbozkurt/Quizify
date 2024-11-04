d6b4cbc962794f20b72de71d0bf3da9144a3b25e
Sedef Bozkurt <sedefbozkurt.4379@gmail.com>
2024-11-04 13:07:43 +0300

    Initial Commit

---
+++ Quizify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizify
+//
+//  Created by Sedef Bozkurt on 4.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
