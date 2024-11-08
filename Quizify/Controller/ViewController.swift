93015667ccc7d8f3ff6429bfebae91e756c33df6
Sedef Bozkurt <sedefbozkurt.4379@gmail.com>
2024-11-08 13:24:41 +0300

    Add UI update functionality to display questions and choices

--- Quizify/Controller/ViewController.swift
+++ Quizify/Controller/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var choice1: UIButton!
+    @IBOutlet weak var choice2: UIButton!
+    @IBOutlet weak var choice3: UIButton!
+    @IBOutlet weak var choice4: UIButton!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var quizBrain = QuizBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
-
-
+    
+    func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        
+        let choices = quizBrain.getChoices()
+        choice1.setTitle(choices[0], for: .normal)
+        choice2.setTitle(choices[1], for: .normal)
+        choice3.setTitle(choices[2], for: .normal)
+        choice4.setTitle(choices[3], for: .normal)
+        
+        scoreLabel.text = "Skor: \(quizBrain.score)"
+    }
 }
 
